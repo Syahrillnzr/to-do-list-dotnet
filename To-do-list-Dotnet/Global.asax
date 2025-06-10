5f17be675c0d8e21886b118859763644bac8e6c0
matRill <syahrill.dev@gmail.com>
2025-06-10 09:47:20 +0800

    first commit

---
+++ To-do-list-Dotnet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="To_do_list_Dotnet.MvcApplication" Language="C#" %>
---
+++ To-do-list-Dotnet/Migrations/202506030426367_InitialCreate.cs
@@ -0,0 +1,27 @@
+﻿namespace To_do_list_Dotnet.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.TodoItems",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Task = c.String(),
+                        IsDone = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.TodoItems");
+        }
+    }
+}
